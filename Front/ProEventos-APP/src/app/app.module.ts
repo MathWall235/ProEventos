0cc2f29133e6825eabdfb2d3c2053a76b6e96aae
Matheus Wallace <matheus.wallacemw17@gmail.com>
2025-01-05 15:19:37 -0300

    Sessão 4: Angular

---
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component'; // Importe o componente
+import { HttpClientModule } from'@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
