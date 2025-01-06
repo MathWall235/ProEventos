ef61041b72766acc71eecb0f38e96e303fbf6ed2
Matheus Wallace <matheus.wallacemw17@gmail.com>
2025-01-06 18:58:16 -0300

    Seção 5: Angular, Interpolação, Diretiva e Bidin

--- Front/ProEventos-APP/src/app/app.module.ts
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { HttpClientModule } from'@angular/common/http'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component'; // Importe o componente
-import { HttpClientModule } from'@angular/common/http'
+import { NavComponent } from './nav/nav.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-  ],
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
